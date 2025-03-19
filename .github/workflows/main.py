22c11f890e71ed4567f90487fbb9af5bf236ed57
zagdali <107754902+zagdali@users.noreply.github.com>
2025-03-19 22:03:31 +0300

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -13,7 +13,7 @@ async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
         await conn.run_sync(models.Base.metadata.create_all)
     yield
-    await engine.dispose()
+    await engine.dispose()  
 
 
 app = FastAPI(lifespan=lifespan,title="Кулинарная книга API",
